fc050b19d57290c712c8843ba8a055c4ae762943
Jonatana RANDRIAMPARANY <103509534+Jonas4884@users.noreply.github.com>
2022-11-25 00:04:45 +0300

    Revert "fix : posts feetlist table title"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import { authProvider } from './provider/authProvider';
+import { authProvider } from './component/authProvider';
 import FeeList from './core/FeeList';
 import { ShowStudentFee } from './core/ShowStudentFee';
-import feeDetails from './pages/list/feesDetails';
 
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider} >
-    <Resource name="posts" list={feeDetails} />
+    <Resource name="posts" list={ListGuesser} />
     <Resource name='users' list={FeeList} show={ShowStudentFee} />
   </Admin>
 );
