7f236ed485f4c0bdf1a9b843c4acb4ed72e11920
Jonas4884 <hei.jonatana@gmail.com>
2022-11-25 00:03:04 +0300

    fix : posts feetlist table title

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import { authProvider } from './component/authProvider';
+import { authProvider } from './provider/authProvider';
 import FeeList from './core/FeeList';
 import { ShowStudentFee } from './core/ShowStudentFee';
+import feeDetails from './pages/list/feesDetails';
 
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider} >
-    <Resource name="posts" list={ListGuesser} />
+    <Resource name="posts" list={feeDetails} />
     <Resource name='users' list={FeeList} show={ShowStudentFee} />
   </Admin>
 );
---
+++ src/pages/list/feesDetails.js
@@ -0,0 +1,11 @@
+import React from'react';
+import FeeDetails from '../../component/datatable';
+
+
+
+const feeDetails = ()=>{
+    return(
+        <FeeDetails />
+    )
+}
+export default feeDetails;
