da89765e99e0811421c99813828a3d0682f73ebb
Jonas4884 <hei.jonatana@gmail.com>
2022-12-15 15:42:43 +0300

    chore(fixed) :user and fees data format

--- src/provider/FakeProvider.js
+++ src/provider/FakeProvider.js
@@ -1,17 +1,35 @@
 import fakeDataProvider from 'ra-data-fakerest';
 
 export const dataProvider = fakeDataProvider({
-    posts:[
+    fees:[
     {
       type: "TUITION",
-    comment: "string",
-    total_amount: 0,
+    comment: "comes to office on 12/12/12",
+    total_amount: 20000,
     creation_datetime: "2022-12-01T11:20:53.885Z",
     due_datetime: "2022-12-01T11:20:53.885Z",
-    id: "string",
+    email: "hei.nahray@gmail.com",
+    id: "1",
+    first_name: "Rakoto",
+      last_name: "Nahary",
+      ref: "STD22020",
     student_id: "string",
     remaining_amount: 0,
     status: "UNPAID"
     }
-  ]
+  ],
+  users : [{
+      id: "1",
+      ref: "STD22020",
+      first_name: "Rakoto",
+      last_name: "Nahary",
+      sex: "M",
+      birth_date: "2022-12-15",
+      address: "IIA 202 TEZ,Ivandry",
+      phone: "+261325285457",
+      email: "hei.nahray@gmail.com",
+      entrance_datetime: "2022-12-15T12:29:12.328Z",
+      status: "ENABLED"
+    
+  }]
   });
